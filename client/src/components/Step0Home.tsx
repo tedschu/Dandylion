e394bfe667ca8d524955f6580ed0486b10bed05b
tedschu <ted.schuster@gmail.com>
2025-05-03 14:16:52 -0500

    updated some of the home page styling

--- client/src/components/Step0Home.tsx
+++ client/src/components/Step0Home.tsx
@@ -21,23 +21,23 @@ function Step0Home({
 
   return (
     <>
-      <div className="homeContainer flexCol">
+      <div className="homeContainer">
         <h1 style={{ marginBottom: "2px" }}>
           TRAVEL PLANNING THAT IS{" "}
           <span style={{ fontStyle: "italic" }}>SO YOU</span>
         </h1>
         <p className="textBackground">
           Whether it's halfway around the world, or just a few hours away -
-          there's a perfect place for you. Keep all the fun of planning your
+          {/* there's a perfect place for you. Keep all the fun of planning your
           next trip, and get rid of the frustration. TRAVEL simply asks you a
           few questions about who you are and what you're looking to do, and
           then it scours the world to find you a trip that you'll love. In just
           five minutes, get detailed recommendations and itineraries for a trip
-          that fits you perfectly.
+          that fits you perfectly. */}
         </p>
 
-        <div className="buttonContainer flexCol">
-          <div className="formContainer flexRow">
+        <div className="buttonContainer">
+          <div className="formContainer">
             <input
               type="text"
               placeholder="First name"
@@ -57,6 +57,7 @@ function Step0Home({
             </button>
           </div>
         </div>
+        <div className="bottomContentContainer">test</div>
       </div>
     </>
   );
